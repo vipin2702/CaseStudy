0ad19f3e1dabd535e3e65c44953d66643cf872e4
vipin2702 <vipinmittal2702@gmail.com>
2019-01-30 22:37:04 +0530

    Create 3.9secondsFinal.java

---
+++ 3.9secondsFinal.java
@@ -0,0 +1,77 @@
+package aisehi2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class App {
+	
+	public static void main(String[] args) throws Exception  {
+
+		FileReader fr = new FileReader("myfile2.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		String str = null;
+
+    long start = System.currentTimeMillis();
+		Path path = Paths.get("myfile2.txt");
+		long lineCount = Files.lines(path).count();
+		System.out.println("Data entries available in file:" + (lineCount-1));
+		
+		if(lineCount==1000001) 
+		{
+			String query = "insert into data2 values(?,?,?,?,?)";
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "system", "system");
+			PreparedStatement ps = con.prepareStatement(query);
+			Statement st = con.createStatement();
+			st.executeUpdate("truncate table data2");
+			
+			
+			final int batchSize = 100000;
+	    int count = 0;
+      
+        while (!(str = br.readLine()).equalsIgnoreCase("Trailer1000001")) {
+				String[] str1 = str.split("\\|");
+				
+				ps.setString(1, str1[0]);
+				ps.setString(2, str1[1]);
+				ps.setString(3, str1[2]);
+				ps.setString(4, str1[3]);
+				ps.setString(5, str1[4]);
+				ps.addBatch();
+				
+				if(++count % batchSize == 0) {
+				ps.executeBatch();
+					
+				}
+			
+			}
+			
+			ps.executeBatch(); // insert remaining records
+			ps.close();
+			con.close();
+			
+			long end = System.currentTimeMillis();
+			System.out.println("Time elapsed = "+(end-start)+"ms");
+			
+		}
+		
+		else {
+			try {
+				throw new Exception() ;	
+			}
+			catch(Exception e) {
+				System.out.println("Exception: The entries in file are either less or more than 1 million records.");
+			}
+		}
+		
+	}
+}
